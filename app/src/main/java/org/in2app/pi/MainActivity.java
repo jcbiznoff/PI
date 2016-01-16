949691510f01620c30012317f3718ffa587a133b
jaychung <jay@max2.com>
2016-01-16 17:50:40 -0500

    udpated navigation drawer icons and images

--- app/src/main/java/org/in2app/pi/MainActivity.java
+++ app/src/main/java/org/in2app/pi/MainActivity.java
@@ -2,9 +2,8 @@ package org.in2app.pi;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -61,12 +61,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_home) {
             return true;
         }
@@ -80,17 +75,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_nurture) {
             // Handle the camera action
+        } else if (id == R.id.nav_worship) {
+
+        } else if (id == R.id.nav_communicatino) {
+
+        } else if (id == R.id.nav_evangelism) {
+
+        } else if (id == R.id.nav_socials) {
+
+        } else if (id == R.id.nav_about_pi) {
+
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
